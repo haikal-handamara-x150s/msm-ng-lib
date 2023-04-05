5493650150948a1721e0b58ba2011a131f72e111
Haikal Handamara <haikal.handamara@siloamhospitals.com>
2023-04-05 08:13:00 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'msm-ng-lib';
+}
